f7bb16bf977939cf930e593560535f7ea4a83a90
Kirill G. Kotenko <zelder.rus@gmail.com>
2016-04-11 14:47:15 +0300

    help up

--- src_mk/fastman2/c51asm.bat
+++ src_mk/fastman2/c51asm.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set asmpath=D:/C51ASM
-set propath=D:/Projects/zeboy2
+set propath=D:/Projects/zeboy2/src_mk/fastman2
 
 cd %asmpath%/BIN/
 c51asm %propath%/fastman2.asm -I %asmpath%/INC/at89c51rc2.inc --target at89c51rc2 --include %propath%/INC
